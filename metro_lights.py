de339328d0ff68414f220d702661cfc8ef671417
Miles Watkins <miles.w.watkins@gmail.com>
2016-08-06 18:37:07 -0400

    Refactor to use new API endpoint

--- metro_lights.py
+++ metro_lights.py
@@ -1,101 +1,69 @@
 from pprint import pprint
+import json
 
 import requests
 
 from config import api_key
 
 
-LINE_INFO_URL = 'https://api.wmata.com/Rail.svc/json/jLines'
-STATION_LIST_URL = 'https://api.wmata.com/Rail.svc/json/jPath'
-STATION_TO_STATION_URL = 'https://api.wmata.com/Rail.svc/json/jSrcStationToDstStationInfo'
-REAL_TIME_URL = 'https://api.wmata.com/StationPrediction.svc/json/GetPrediction/All'
+# STATIONS_URL = 'https://api.wmata.com/Rail.svc/json/jLines'
+ROUTES_URL = 'https://api.wmata.com/TrainPositions/StandardRoutes?contentType=json'
+POSITIONS_URL = 'https://api.wmata.com/TrainPositions/TrainPositions?contentType=json'
+HEADERS = {'api_key': api_key}
 
-# Get metadata on each line
-data = requests.get(LINE_INFO_URL, headers={'api_key': api_key}).json()
-lines = {x['LineCode'] : x for x in data['Lines']}
+# stations = json.loads(requests.get(STATIONS_URL, headers=HEADERS).text)
 
-for (line, line_info) in lines.iteritems():
-    # Get every station on the line
-    station_list_query_string = {
-        'FromStationCode': line_info['StartStationCode'],
-        'ToStationCode': line_info['EndStationCode']
-    }
-    data = requests.get(
-        STATION_LIST_URL,
-        params=station_list_query_string,
-        headers={'api_key': api_key}
-    ).json()
-    lines[line]['Route'] = data['Path']
+routes = json.loads(requests.get(ROUTES_URL, headers=HEADERS).text)['StandardRoutes']
 
-    # Get scheduled time between each station
-    for station_num in range(1, len(lines[line]['Route'])):
-        station_to_station_query_string = {
-            'FromStationCode': lines[line]['Route'][station_num - 1]['StationCode'],
-            'ToStationCode': lines[line]['Route'][station_num]['StationCode']
-        }
-        data = requests.get(
-            STATION_TO_STATION_URL,
-            params=station_to_station_query_string,
-            headers={'api_key': api_key}
-        ).json()
-        lines[line]['Route'][station_num]['TimeToPrev'] = data['StationToStationInfos'][0]['RailTime']
-    lines[line]['Route'][0]['TimeToPrev'] = 0
-    for station in lines[line]['Route']:
-        del station['LineCode']
-        del station['SeqNum']
-        # Interesting information, but probably not necessary for this project
-        del station['DistanceToPrev']
+positions = json.loads(requests.get(POSITIONS_URL, headers=HEADERS).text)['TrainPositions']
+positions = [
+    p for p in positions if
+    p['ServiceType'] in ('Normal', 'Special') and
+    p['CarCount'] != 0 and
+    p['DestinationStationCode'] is not None
+]
 
-    lines[line]['EndStationCodes'] = [
-        x for x in [
-            lines[line].pop('EndStationCode'),
-            lines[line].pop('InternalDestination1'),
-            lines[line].pop('InternalDestination2')
-        ] if x
-    ]
-    del lines[line]['LineCode']
+for train in positions:
+    previous_station_circuit = None
+    current_circuit = None
+    next_station_circuit = None
+    destination_is_behind = False
 
-# pprint(lines)
+    for route in [r for r in routes if r['LineCode'] == train['LineCode']]:
+        for circuit in route['TrackCircuits']:
+            # If solution has already been found going the other direction, skip this direction
+            if previous_station_circuit and next_station_circuit:
+                break
 
-# Get the real-time data from WMATA
-data = requests.get(REAL_TIME_URL, headers={'api_key': api_key}).json()
+            if circuit['StationCode'] == train['DestinationStationCode']:
+                destination_is_behind = True
+            if circuit['CircuitId'] == train['CircuitId']:
+                current_circuit = circuit
+            if circuit['StationCode'] is not None:
+                if current_circuit is None:
+                    previous_station_circuit = circuit
+                else:
+                    next_station_circuit = circuit
+                    break
 
-# Clean the data, filter out bad information
-BAD_DESTINATION_CODES = (None, )
-BAD_MIN_CODES = ('---', )
-ZERO_MINUTE_CODES = ('ARR', 'BRD')
-
-trains = data['Trains']
-clean_trains = []
-for train in trains:
-    if train['DestinationCode'] in BAD_DESTINATION_CODES or \
-            train['Min'] in BAD_MIN_CODES:
+    if previous_station_circuit is None or next_station_circuit is None:
         continue
-    if train['Min'] in ZERO_MINUTE_CODES:
-        train['Min'] = '0'
-    train['Min'] = int(train['Min'])
+    if destination_is_behind:
+        (previous_station_circuit, next_station_circuit) = (next_station_circuit, previous_station_circuit)
 
-    # Ignore trains in waiting at terminus stations
-    if train['Car'] is None:
-        continue
-    # I believe that "2-car" trains are 7000-series cars
-    train['Car'] = int(train['Car'])
+    # print('previous', previous_station_circuit)
+    # print('current', current_circuit)
+    # print('next', next_station_circuit)
 
-    clean_trains.append(train)
-
-# pprint(clean_trains)
-
-for train in clean_trains:
-    line = lines[train['Line']]
-
-    # For now, skip trains that are going in the opposite direction
-    if train['DestinationCode'] not in line['EndStationCodes']:
-        continue
-
-    (station_info, ) = [x for x in line['Route'] if x['StationCode'] == train['LocationCode']]
-    percentage_of_way_to_station = float(train['Min']) / float(station_info['TimeToPrev'])
-
-    print('---')
-    print(train)
-    print(station_info)
-    print(percentage_of_way_to_station)
+    progress = (
+        float(current_circuit['SeqNum'] - previous_station_circuit['SeqNum']) /
+        float(next_station_circuit['SeqNum'] - previous_station_circuit['SeqNum'])
+    )
+    print(
+        "{LineCode} Line train {TrainId} is {progress_pct}% of the way from {previous_station} to {next_station}".format(**{
+            'LineCode': train['LineCode'],
+            'TrainId': train['TrainId'],
+            'progress_pct': int(progress * 100),
+            'previous_station': previous_station_circuit['StationCode'],
+            'next_station': next_station_circuit['StationCode']
+    }))
