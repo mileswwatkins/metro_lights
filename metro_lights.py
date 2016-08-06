fc3c5f3d90588097b8ac962abfd0cde09357b2ba
Miles Watkins <miles.w.watkins@gmail.com>
2016-08-06 18:47:13 -0400

    Add station name print

--- metro_lights.py
+++ metro_lights.py
@@ -1,29 +1,36 @@
 from pprint import pprint
 import json
+from time import sleep
 
 import requests
 
 from config import api_key
 
 
-# STATIONS_URL = 'https://api.wmata.com/Rail.svc/json/jLines'
+STATIONS_URL = 'https://api.wmata.com/Rail.svc/json/jStationInfo?StationCode={}'
 ROUTES_URL = 'https://api.wmata.com/TrainPositions/StandardRoutes?contentType=json'
 POSITIONS_URL = 'https://api.wmata.com/TrainPositions/TrainPositions?contentType=json'
 HEADERS = {'api_key': api_key}
 
-# stations = json.loads(requests.get(STATIONS_URL, headers=HEADERS).text)
+def get_station_name(station_code):
+    return json.loads(requests.get(STATIONS_URL.format(station_code), headers=HEADERS).text)['Name']
 
 routes = json.loads(requests.get(ROUTES_URL, headers=HEADERS).text)['StandardRoutes']
 
 positions = json.loads(requests.get(POSITIONS_URL, headers=HEADERS).text)['TrainPositions']
 positions = [
     p for p in positions if
-    p['ServiceType'] in ('Normal', 'Special') and
-    p['CarCount'] != 0 and
-    p['DestinationStationCode'] is not None
+        p['ServiceType'] in ('Normal', 'Special') and
+        p['CarCount'] > 0 and
+        p['DestinationStationCode'] is not None
 ]
 
 for train in positions:
+    # Avoid API rate-limiting
+    # Only necessary because we're individually fetching the station name
+    # Won't be an issue later on
+    sleep(1)
+
     previous_station_circuit = None
     current_circuit = None
     next_station_circuit = None
@@ -64,6 +71,6 @@ for train in positions:
             'LineCode': train['LineCode'],
             'TrainId': train['TrainId'],
             'progress_pct': int(progress * 100),
-            'previous_station': previous_station_circuit['StationCode'],
-            'next_station': next_station_circuit['StationCode']
+            'previous_station': get_station_name(previous_station_circuit['StationCode']),
+            'next_station': get_station_name(next_station_circuit['StationCode'])
     }))
